7193ddb707b16ad4226a53628015258b9dc9a5be
Nathan <sisu.empcoaching@gmail.com>
2024-11-22 15:06:10 -0500

    Excellence

---
+++ my-server/migrations/01_20241122193224_create_events_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('events', (table) => {
+        table.increments('id').primary();
+        table.string('name');
+        table.string('location');
+        table.string('password');
+        table.date('date');
+        table.time('time');
+    });
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('events');
+};
