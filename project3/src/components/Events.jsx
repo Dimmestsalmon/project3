05f0fc644a8dd31ec260ba53581ec1de8ee14d4a
Breeann <breeann.doremus@spaceforce.mil>
2024-11-25 17:18:46 -0500

    add events almost done

--- project3/src/components/Events.jsx
+++ project3/src/components/Events.jsx
@@ -26,37 +26,82 @@ const Events = () => {
   // for input changes
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setNewEvent((prevEvent) => ({
-      ...prevEvent,
+    setNewEvent((prevEvents) => ({
+      ...prevEvents,
       [name]: value,
     }));
   };
-  // const addUser = (newUser) =>{
-  //   console.log('fetching')
-  //     fetch('http://localhost:8080/users', {
-  //       method: "POST",
-  //       body: JSON.stringify({ "name": newUser })
+
+
+  // // add new events
+  // const addEvent = () => {
+  //   const { name, location, date, time } = newEvent;
+  //   if (!name || !location || !data || !time) {
+  //     alert("All fields must be populated")
+  //   return;
+  //   }
+
+
+  //   fetch('http://localhost:8080/events', {
+  //     method: "POST",
+  //     headers:{
+  //       "Content-Type":"application/json"
+  //     },
+  //     body: JSON.stringify({name:newEvent})
+  //   })
+  //   .then((res) => {
+  //     if(!res.ok) {
+  //       throw new Error("Failed to add event.")
+  //     }
+  //     return res.json();
+  //   })
+  //   .then((addedEvent) => {
+  //     setEvents((prevEvents) => [...prevEvents, addedEvent]);
+  //     setNewEvent({
+  //       name: '',
+  //       location: '',
+  //       date: '',
+  //       time:''
   //     })
-  //     .then(data => console.log(data))
-  // }
-  useEffect(()=>{
-      fetch('http://localhost:8080/users')
-      .then(res => res.json())
-      .then (data => setUsers(data))
-  }, [])
-  if(users){
-  return (
-    <>
-    <h1>List of Users</h1>
-    <input type = 'text' placeholder = 'Name here' onChange = {inputUser}/>
-    <button onClick={() => addUser(newUser)}>Add User</button>
-    {users.map((user, index) => {
-      return (
-      <p key = {index}>{user.name}</p>
-      )
-    })}
-    </>
-  )
-}
-}
+  //     .catch((err) => setError(err.message));
+  //   }
+  //   if (error){
+  //     return <p>Error:{error}</p>
+  //   }
+
+
+
+
+
+  if(events){
+    return (
+      <>
+      <h1>New Event</h1>
+
+      </>
+    )
+  }
+
+
+
+//   useEffect(()=>{
+//       fetch('http://localhost:8080/users')
+//       .then(res => res.json())
+//       .then (data => setUsers(data))
+//   }, [])
+//   if(users){
+//   return (
+//     <>
+//     <h1>List of Users</h1>
+//     <input type = 'text' placeholder = 'Name here' onChange = {inputUser}/>
+//     <button onClick={() => addUser(newUser)}>Add User</button>
+//     {users.map((user, index) => {
+//       return (
+//       <p key = {index}>{user.name}</p>
+//       )
+//     })}
+//     </>
+//   )
+// }
+// }
 export default Events
