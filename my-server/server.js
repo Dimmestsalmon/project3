7e44cbd78cfc190bcac7385161942e5aa6c3e1ce
Nathan <sisu.empcoaching@gmail.com>
2024-11-25 11:38:48 -0500

    Nathan fucked something up

--- my-server/server.js
+++ my-server/server.js
@@ -6,5 +6,10 @@ const knex = require('knex')(require('./knexfile.js')['development'])
 
 
 server.get('/', (req, res) => res.send('Sasquatch is Real!'))
+server.get('/events', (req, res) => {
+    knex.select('*').from('events')
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(400).send('Error fetching events'))
+})
 
 server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
