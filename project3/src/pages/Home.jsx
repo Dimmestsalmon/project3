7eab6f39b41032eb1c02e5ab7b2fc85ff07eadcb
Breeann <breeann.doremus@spaceforce.mil>
2024-11-26 16:11:53 -0500

    "committed changes from origin to group 1 branch"

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+
+  return (
+    <div className="home-page">
+      <h1 className="header">Queue Buddy</h1>
+      <Link className="join-queue" to="/queue">Join A Queue</Link>
+      <Link className="available-events" to="/events">Available Events</Link>
+    </div>
+  );
+};
+
+export default Home;
+
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -5,8 +21,15 @@ const Home = () => {
   return (
     <div>
       <h1>Queue Buddy</h1>
-      <Link to="/queue">Join A Queue</Link>
-      <Link to="/events">Available Events</Link>
+      <p>
+        <Link to="/queue">Queue List</Link>
+      </p>
+      <p>
+        <Link to="/events">Available Events</Link>
+      </p>
+      <p>
+        <Link to="/users">List of Users</Link>
+      </p>
     </div>
   );
 };
