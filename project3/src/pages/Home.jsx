287c670d795db79663b85c76c02b4ea58a27e038
Breeann <breeann.doremus@spaceforce.mil>
2024-11-26 15:51:38 -0500

    Home Page styled

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Home = () => {
 
   return (
-    <div>
-      <h1>Queue Buddy</h1>
-      <Link to="/queue">Join A Queue</Link>
-      <Link to="/events">Available Events</Link>
+    <div className="home-page">
+      <h1 className="header">Queue Buddy</h1>
+      <Link className="join-queue" to="/queue">Join A Queue</Link>
+      <Link className="available-events" to="/events">Available Events</Link>
     </div>
   );
 };
