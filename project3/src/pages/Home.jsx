c42e990bb49aabbf14cce3ddb2d829a4cf043135
Breeann <breeann.doremus@spaceforce.mil>
2024-11-26 16:15:27 -0500

    "kept current Home"

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -1,19 +1,3 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Home = () => {
-
-  return (
-    <div className="home-page">
-      <h1 className="header">Queue Buddy</h1>
-      <Link className="join-queue" to="/queue">Join A Queue</Link>
-      <Link className="available-events" to="/events">Available Events</Link>
-    </div>
-  );
-};
-
-export default Home;
-
 import React from "react";
 import { Link } from "react-router-dom";
 
